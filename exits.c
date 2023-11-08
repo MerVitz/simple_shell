0a1dfc771b43fd4fb953d638cd37e0cd88e0ac18
Amakalu Vitalis <amakaluvitalis202@gmail.com>
2023-11-08 22:02:52 +0300

    Update exits.c

--- exits.c
+++ exits.c
@@ -1,74 +1,73 @@
 #include "shell.h"
 
 /**
- **_strncpy - copies a string
- *@dest: the destination string to be copied to
- *@src: the source string
- *@n: the amount of characters to be copied
- *Return: the concatenated string
- */
-char *_strncpy(char *dest, char *src, int n)
-{
-	int i, j;
-	char *s = dest;
-
-	i = 0;
-	while (src[i] != '\0' && i < n - 1)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	if (i < n)
-	{
-		j = i;
-		while (j < n)
-		{
-			dest[j] = '\0';
-			j++;
-		}
-	}
-	return (s);
-}
-
-/**
- **_strncat - concatenates two strings
- *@dest: the first string
- *@src: the second string
- *@n: the amount of bytes to be maximally used
- *Return: the concatenated string
- */
-char *_strncat(char *dest, char *src, int n)
-{
-	int i, j;
-	char *s = dest;
-
-	i = 0;
-	j = 0;
-	while (dest[i] != '\0')
-		i++;
-	while (src[j] != '\0' && j < n)
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	if (j < n)
-		dest[i] = '\0';
-	return (s);
-}
-
-/**
- **_strchr - locates a character in a string
- *@s: the string to be parsed
- *@c: the character to look for
- *Return: (s) a pointer to the memory area s
+ * locate_char - searches for the first occurrence of a character in a string
+ * @s: the string to be searched
+ * @c: the character to find
+ *
+ * This function scans a string for a specific character and returns a pointer
+ * to the first occurrence of this character in the string.
  */
 char *_strchr(char *s, char c)
 {
 	do {
 		if (*s == c)
 			return (s);
-	} while (*s++ != '\0');
+	} while (*s++);
 
 	return (NULL);
+}
+
+/**
+ * string_concatenate - merges a certain amount of characters from one string to another
+ * @dest: the string to be appended to
+ * @src: the string to append from
+ * @n: the maximum number of characters to append
+ *
+ * Appends up to n characters from the source to the destination string. 
+ * If the source is shorter than n, the remaining space in the destination will be null-padded.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *result = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i])
+		i++;
+	while (src[j] && j < n)
+	{
+		dest[i++] = src[j++];
+	}
+	if (j < n)
+		dest[i] = '\0';
+
+	return (result);
+}
+
+/**
+ * string_copy_n - duplicates a specified number of characters from a string
+ * @dest: the buffer to copy to
+ * @src: the source string to copy from
+ * @n: the number of characters to copy
+ *
+ * Copies up to n characters from one string to another. If the source string is shorter
+ * than n, the remainder of the destination string is filled with null bytes.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *result = dest;
+
+	i = 0;
+	while (src[i] && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	for (j = i; j < n; j++)
+		dest[j] = '\0';
+
+	return (result);
 }
