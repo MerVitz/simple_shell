62d3162177dd0e8026285df1807f480fc7a8da71
Amakalu Vitalis <amakaluvitalis202@gmail.com>
2023-11-08 16:42:02 +0300

    Update string1.c

--- string1.c
+++ string1.c
@@ -1,32 +1,52 @@
 #include "shell.h"
 
 /**
- * _strcpy - copies a string
- * @dest: the destination
- * @src: the source
+ * char_output - outputs a single character to standard output
+ * @c: the character to be printed
  *
- * Return: pointer to destination
+ * This function buffers characters and prints them in a batch to improve performance.
+ * The buffer is flushed either when it's full or when a flush command is received.
  */
-char *_strcpy(char *dest, char *src)
+int _putchar(char c)
 {
-	int i = 0;
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
 
-	if (dest == src || src == 0)
-		return (dest);
-	while (src[i])
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
 	{
-		dest[i] = src[i];
-		i++;
+		write(1, buf, i);
+		i = 0;
 	}
-	dest[i] = 0;
-	return (dest);
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
 }
 
 /**
- * _strdup - duplicates a string
- * @str: the string to duplicate
+ * print_string - displays a string to standard output
+ * @str: the string to be output
  *
- * Return: pointer to the duplicated string
+ * This function sends a string to the _putchar function character by character.
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * string_clone - creates a copy of the given string
+ * @str: the string to be cloned
+ *
+ * Allocates memory for a new copy of the string, and returns a pointer to it.
+ * The memory must be freed by the caller.
  */
 char *_strdup(const char *str)
 {
@@ -46,42 +66,24 @@ char *_strdup(const char *str)
 }
 
 /**
- *_puts - prints an input string
- *@str: the string to be printed
+ * string_copy - copies a string from source to destination
+ * @dest: buffer to copy the string to
+ * @src: the string to be copied
  *
- * Return: Nothing
+ * After copying, the destination string is returned. The caller must ensure
+ * that the destination buffer is large enough to hold the source string.
  */
-void _puts(char *str)
+char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	if (!str)
-		return;
-	while (str[i] != '\0')
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[i])
 	{
-		_putchar(str[i]);
+		dest[i] = src[i];
 		i++;
 	}
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	static int i;
-	static char buf[WRITE_BUF_SIZE];
-
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-	{
-		write(1, buf, i);
-		i = 0;
-	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
-	return (1);
+	dest[i] = 0;
+	return (dest);
 }
