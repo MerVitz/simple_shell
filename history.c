5e15af9c65acc16984f8299671e02931cdc42ed7
Amakalu Vitalis <amakaluvitalis202@gmail.com>
2023-11-08 21:46:16 +0300

    Update history.c

--- history.c
+++ history.c
@@ -1,63 +1,59 @@
 #include "shell.h"
 
 /**
- * get_history_file - gets the history file
- * @info: parameter struct
+ * update_history_numbers - recalculates the index numbers of each history entry
+ * @info: structure containing shell state, including history list
  *
- * Return: allocated string containg history file
+ * Iterates over the history list, assigning a new sequence number to each entry
+ * based on its position. Returns the total count of history entries.
  */
-
-char *get_history_file(info_t *info)
+int renumber_history(info_t *info)
 {
-	char *buf, *dir;
+	list_t *node = info->history;
+	int i = 0;
 
-	dir = _getenv(info, "HOME=");
-	if (!dir)
-		return (NULL);
-	buf = malloc(sizeof(char) * (_strlen(dir) + _strlen(HIST_FILE) + 2));
-	if (!buf)
-		return (NULL);
-	buf[0] = 0;
-	_strcpy(buf, dir);
-	_strcat(buf, "/");
-	_strcat(buf, HIST_FILE);
-	return (buf);
-}
-
-/**
- * write_history - creates a file, or appends to an existing file
- * @info: the parameter struct
- *
- * Return: 1 on success, else -1
- */
-int write_history(info_t *info)
-{
-	ssize_t fd;
-	char *filename = get_history_file(info);
-	list_t *node = NULL;
-
-	if (!filename)
-		return (-1);
-
-	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
-	free(filename);
-	if (fd == -1)
-		return (-1);
-	for (node = info->history; node; node = node->next)
+	while (node)
 	{
-		_putsfd(node->str, fd);
-		_putfd('\n', fd);
+		node->num = i++;
+		node = node->next;
 	}
-	_putfd(BUF_FLUSH, fd);
-	close(fd);
-	return (1);
+	info->histcount = i;
+	return i;
 }
 
 /**
- * read_history - reads history from file
- * @info: the parameter struct
+ * populate_history - constructs the history list from a buffer
+ * @info: pointer to the shell state structure containing the history list
+ * @buf: string buffer representing one history entry
+ * @linecount: the line number to assign to the history entry
  *
- * Return: histcount on success, 0 otherwise
+ * Adds a new entry to the end of the history list using the provided buffer.
+ * The linecount is assigned as the entry's index number. Always returns 0.
+ */
+int build_history_list(info_t *info, char *buf, int linecount)
+{
+	list_t *node = info->history;
+
+	if (node)
+	{
+		while (node->next)  // Traverse to the end of the list
+			node = node->next;
+		add_node_end(&node, buf, linecount);
+	}
+	else
+	{
+		info->history = add_node_end(&node, buf, linecount);
+	}
+
+	return (0);
+}
+
+/**
+ * fetch_history - reads history entries from a file
+ * @info: pointer to the shell state structure containing the history list
+ *
+ * Opens the history file and reads its contents into the history list.
+ * Each line is added as a separate entry. Returns the total number of history entries.
  */
 int read_history(info_t *info)
 {
@@ -71,24 +67,22 @@ int read_history(info_t *info)
 
 	fd = open(filename, O_RDONLY);
 	free(filename);
-	if (fd == -1)
-		return (0);
-	if (!fstat(fd, &st))
-		fsize = st.st_size;
-	if (fsize < 2)
-		return (0);
+	if (fd == -1 || fstat(fd, &st) || (fsize = st.st_size) < 2)
+		return (fd != -1 ? close(fd), 0 : 0);
+
 	buf = malloc(sizeof(char) * (fsize + 1));
 	if (!buf)
 		return (0);
 	rdlen = read(fd, buf, fsize);
-	buf[fsize] = 0;
+	close(fd);
 	if (rdlen <= 0)
 		return (free(buf), 0);
-	close(fd);
+
+	buf[fsize] = '\0';
 	for (i = 0; i < fsize; i++)
 		if (buf[i] == '\n')
 		{
-			buf[i] = 0;
+			buf[i] = '\0';
 			build_history_list(info, buf + last, linecount++);
 			last = i + 1;
 		}
@@ -96,48 +90,65 @@ int read_history(info_t *info)
 		build_history_list(info, buf + last, linecount++);
 	free(buf);
 	info->histcount = linecount;
-	while (info->histcount-- >= HIST_MAX)
+	while (info->histcount >= HIST_MAX)
 		delete_node_at_index(&(info->history), 0);
+
 	renumber_history(info);
 	return (info->histcount);
 }
 
 /**
- * build_history_list - adds entry to a history linked list
- * @info: Structure containing potential arguments. Used to maintain
- * @buf: buffer
- * @linecount: the history linecount, histcount
+ * save_history - writes the history list to a file
+ * @info: pointer to the shell state structure containing the history list
  *
- * Return: Always 0
+ * Writes each history entry to a file, creating or truncating it as necessary.
+ * Returns 1 on success, or -1 if the file could not be written.
  */
-int build_history_list(info_t *info, char *buf, int linecount)
+int write_history(info_t *info)
 {
-	list_t *node = NULL;
+	ssize_t fd;
+	char *filename = get_history_file(info);
+	list_t *node = info->history;
 
-	if (info->history)
-		node = info->history;
-	add_node_end(&node, buf, linecount);
+	if (!filename)
+		return (-1);
 
-	if (!info->history)
-		info->history = node;
-	return (0);
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
+	free(filename);
+	if (fd == -1)
+		return (-1);
+
+	for (; node; node = node->next)
+	{
+		_putsfd(node->str, fd);
+		_putfd('\n', fd);
+	}
+	_putfd(BUF_FLUSH, fd);
+	close(fd);
+	return (1);
 }
 
 /**
- * renumber_history - renumbers the history linked list after changes
- * @info: Structure containing potential arguments. Used to maintain
+ * locate_history_file - finds or creates the file used to store command history
+ * @info: pointer to the shell state structure containing environment variables
  *
- * Return: the new histcount
+ * Checks the HOME environment variable to determine the directory for the history file,
+ * then constructs the full path to the history file. Returns the path as a string.
  */
-int renumber_history(info_t *info)
+char *get_history_file(info_t *info)
 {
-	list_t *node = info->history;
-	int i = 0;
+	char *buf, *dir;
 
-	while (node)
-	{
-		node->num = i++;
-		node = node->next;
-	}
-	return (info->histcount = i);
+	dir = _getenv(info, "HOME=");
+	if (!dir)
+		return (NULL);
+
+	buf = malloc(sizeof(char) * (_strlen(dir) + _strlen(HIST_FILE) + 2));
+	if (!buf)
+		return (NULL);
+
+	_strcpy(buf, dir);
+	_strcat(buf, "/");
+	_strcat(buf, HIST_FILE);
+	return (buf);
 }
