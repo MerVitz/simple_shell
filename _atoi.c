6c56327f10353f70ac1b121ae5b4699492bf1a9d
Amakalu Vitalis <amakaluvitalis202@gmail.com>
2023-11-09 06:40:34 +0300

    Update _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -1,74 +1,72 @@
 #include "shell.h"
 
 /**
- * interactive - returns true if shell is interactive mode
- * @info: struct address
+ * convert_string_to_int - converts string to integer, ignoring non-numeric leading characters
+ * @s: the string that represents an integer
  *
- * Return: 1 if interactive mode, 0 otherwise
+ * Parses the string to extract an integer value. Handles negative numbers by detecting a '-' sign.
+ * The conversion continues until a non-digit character is encountered if an initial digit has been identified.
  */
-int interactive(info_t *info)
-{
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
-}
-
-/**
- * is_delim - checks if character is a delimeter
- * @c: the char to check
- * @delim: the delimeter string
- * Return: 1 if true, 0 if false
- */
-int is_delim(char c, char *delim)
-{
-	while (*delim)
-		if (*delim++ == c)
-			return (1);
-	return (0);
-}
-
-/**
- *_isalpha - checks for alphabetic character
- *@c: The character to input
- *Return: 1 if c is alphabetic, 0 otherwise
- */
-
-int _isalpha(int c)
-{
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
-}
-
-/**
- *_atoi - converts a string to an integer
- *@s: the string to be converted
- *Return: 0 if no numbers in string, converted number otherwise
- */
-
 int _atoi(char *s)
 {
 	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
-	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
 	{
 		if (s[i] == '-')
-			sign *= -1;
+			sign *= -1; // Toggle the sign with each '-' found.
 
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			flag = 1;
-			result *= 10;
-			result += (s[i] - '0');
+			flag = 1; // Mark that we've started parsing a number.
+			result = result * 10 + (s[i] - '0'); // Build the integer result.
 		}
-		else if (flag == 1)
+		else if (flag == 1) // If we've already started a number, stop converting on non-digit.
 			flag = 2;
 	}
 
-	if (sign == -1)
-		output = -result;
-	else
-		output = result;
-
+	output = sign == -1 ? -result : result; // Apply the sign to the result.
 	return (output);
+}
+
+/**
+ * check_if_alpha - determines if a character is an alphabetic letter
+ * @c: the character to be checked
+ *
+ * Returns 1 if the character is a letter, 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1); // Check for both lowercase and uppercase alphabetic characters.
+	else
+		return (0);
+}
+
+/**
+ * verify_delimiter - checks if a character is among a set of delimiters
+ * @c: the character to check
+ * @delim: the delimiter string
+ *
+ * Returns 1 if the character is a delimiter, 0 otherwise.
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1); // Check against each character in the delimiter string.
+	return (0);
+}
+
+/**
+ * check_interactive_mode - assesses if the shell is running in interactive mode
+ * @info: shell state information
+ *
+ * Determines if the shell is interacting with a user or running from a script by checking the file descriptor.
+ * Returns 1 for interactive mode, 0 for non-interactive.
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2); // Check if STDIN is connected to a terminal.
 }
