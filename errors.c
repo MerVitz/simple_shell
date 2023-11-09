f0612fe96acdef15deac5ee13a74369fa128961b
Amakalu Vitalis <amakaluvitalis202@gmail.com>
2023-11-09 06:14:05 +0300

    Update errors.c

--- errors.c
+++ errors.c
@@ -1,85 +1,79 @@
 #include "shell.h"
 
 /**
- *_eputs - prints an input string
- * @str: the string to be printed
+ * write_to_file_descriptor - writes a single character to a specified file descriptor
+ * @c: the character to output
+ * @fd: the file descriptor to which the character will be written
  *
- * Return: Nothing
- */
-void _eputs(char *str)
-{
-	int i = 0;
-
-	if (!str)
-		return;
-	while (str[i] != '\0')
-	{
-		_eputchar(str[i]);
-		i++;
-	}
-}
-
-/**
- * _eputchar - writes the character c to stderr
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _eputchar(char c)
-{
-	static int i;
-	static char buf[WRITE_BUF_SIZE];
-
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-	{
-		write(2, buf, i);
-		i = 0;
-	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
-	return (1);
-}
-
-/**
- * _putfd - writes the character c to given fd
- * @c: The character to print
- * @fd: The filedescriptor to write to
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Buffers the character and writes it to the specified file descriptor. The buffer is flushed when full or when commanded.
+ * Returns 1 on success, or -1 if an error occurs and sets errno.
  */
 int _putfd(char c, int fd)
 {
-	static int i;
+	static int i = 0;
 	static char buf[WRITE_BUF_SIZE];
 
 	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
 	{
-		write(fd, buf, i);
-		i = 0;
+		write(fd, buf, i); // Flush the buffer to the file descriptor.
+		i = 0; // Reset the buffer index.
 	}
 	if (c != BUF_FLUSH)
-		buf[i++] = c;
+		buf[i++] = c; // Store the character in the buffer.
 	return (1);
 }
 
 /**
- *_putsfd - prints an input string
- * @str: the string to be printed
- * @fd: the filedescriptor to write to
+ * print_string_to_fd - outputs a string to a given file descriptor
+ * @str: the string to print
+ * @fd: the file descriptor to write to
  *
- * Return: the number of chars put
+ * This function sends each character of a string to the _putfd function to write to the specified file descriptor.
+ * Returns the total number of characters written.
  */
 int _putsfd(char *str, int fd)
 {
-	int i = 0;
+	int count = 0;
 
-	if (!str)
-		return (0);
-	while (*str)
+	while (str && *str)
 	{
-		i += _putfd(*str++, fd);
+		count += _putfd(*str++, fd); // Write each character to the file descriptor.
 	}
-	return (i);
+	return (count); // Return the number of characters written.
+}
+
+/**
+ * print_error_character - writes a single character to stderr
+ * @c: the character to output
+ *
+ * Buffers the character and writes it to standard error. The buffer is flushed when full or when commanded.
+ * Returns 1 on success, or -1 on error with errno set appropriately.
+ */
+int _eputchar(char c)
+{
+	static int i = 0;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i); // Flush the buffer to stderr.
+		i = 0; // Reset the buffer index.
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c; // Store the character in the buffer.
+	return (1);
+}
+
+/**
+ * print_error_string - outputs an error message to stderr
+ * @str: the error message to print
+ *
+ * Iterates through the error message string, passing each character to the _eputchar function to write to stderr.
+ */
+void _eputs(char *str)
+{
+	while (str && *str)
+	{
+		_eputchar(*str++); // Write each character to stderr.
+	}
 }
