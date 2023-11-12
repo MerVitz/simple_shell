1658a9ae809c3b97f0800d09369c622f5f687572
Amakalu Vitalis <amakaluvitalis202@gmail.com>
2023-11-12 15:01:17 +0300

    Update memory.c

--- memory.c
+++ memory.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * safe_free - releases allocated memory and resets the pointer
+ * bfree - releases allocated memory and resets the pointer
  * @ptr: a double pointer referencing the memory to be released
  *
  * Return: 1 on successful release of memory, 0 if not executed.
@@ -10,9 +10,9 @@ int bfree(void **ptr)
 {
 	if (ptr && *ptr)
 	{
-		free(*ptr); // Deallocate memory block
-		*ptr = NULL; // Set the pointer to NULL to avoid dangling references
-		return (1); // Indicate successful deallocation
+		free(*ptr); 
+		*ptr = NULL; 
+		return (1);
 	}
 	return (0); // Return 0 if no action was taken
 }
