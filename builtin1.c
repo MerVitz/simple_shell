9761b174b602a9e5daa1d62873ef7ffc006b8a4b
Amakalu Vitalis <amakaluvitalis202@gmail.com>
2023-11-12 21:36:15 +0300

    Update builtin1.c

--- builtin1.c
+++ builtin1.c
@@ -4,13 +4,13 @@
  * display_history - outputs the history list with line numbers
  * @info: structure containing shell state, including the history list
  *
- * Iterates through the history list and prints each command with its corresponding line number.
- * Returns 0 after printing the entire history list.
+ * Iterates through the history list and prints each command 
+ * with its corresponding line number.
+ * Return: 0 after printing the entire history list.
  */
-int _myhistory(info_t *info)
+int display_history(info_t *info)
 {
-	print_list(info->history); // Calls print_list to display the history entries.
-	return (0);
+	print_list(info->history); 
 }
 
 /**
@@ -18,23 +18,25 @@ int _myhistory(info_t *info)
  * @info: structure containing shell state, including the alias list
  * @str: string that represents the alias to be removed
  *
- * Separates the alias name from its value and removes the alias from the list if found.
- * Returns 0 on successful removal, 1 if the alias was not found or if an error occurred.
+ * Separates the alias name from its value and removes the alias
+ * from the list if found.
+ * Return: 0 on successful removal, 1 if the alias was not found
+ * or if an error occurred.
  */
-int unset_alias(info_t *info, char *str)
+int remove_alias(info_t *info, char *str)
 {
 	char *equal_sign_position;
 	int result;
 
 	equal_sign_position = _strchr(str, '=');
 	if (!equal_sign_position)
-		return (1); // If there's no '=' character, it's not a valid alias, return error.
+		return (1);
 
-	*equal_sign_position = '\0'; // Temporarily terminate the string at the '=' character to isolate the alias name.
+	*equal_sign_position = '\0';
 	result = delete_node_at_index(&(info->alias),
 		get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
-	*equal_sign_position = '='; // Restore the '=' character after operation.
-	return (result); // Return the result of the deletion operation.
+	*equal_sign_position = '=';
+	return (result);
 }
 
 /**
@@ -42,32 +44,35 @@ int unset_alias(info_t *info, char *str)
  * @info: structure containing shell state, including the alias list
  * @str: string that represents the alias and its value
  *
- * Analyzes the given string and either creates a new alias or updates an existing one.
- * Returns 0 on success, 1 on failure to set the alias.
+ * Analyzes the given string and either creates a new alias
+ * or updates an existing one.
+ * Return: 0 on success, 1 on failure to set the alias.
  */
-int set_alias(info_t *info, char *str)
+int create_or_update_alias(info_t *info, char *str)
 {
 	char *equal_sign_position;
 
 	equal_sign_position = _strchr(str, '=');
 	if (!equal_sign_position)
-		return (1); // If '=' is not found, return error.
+		return (1);
 
 	if (!*(equal_sign_position + 1))
-		return (unset_alias(info, str)); // If there's no value after '=', it's an unset operation.
+		return (unset_alias(info, str));
 
-	unset_alias(info, str); // Remove any existing alias with the same name.
-	return (add_node_end(&(info->alias), str, 0) == NULL); // Attempt to add the new alias and return the result.
+	unset_alias(info, str);
+	return (add_node_end(&(info->alias), str, 0) == NULL);
 }
 
 /**
  * output_alias - prints a formatted representation of an alias
  * @node: the list node containing the alias string
  *
- * Formats and prints an alias in the form 'name=value'. If the node is NULL, returns 1.
- * Otherwise, it returns 0 after printing the alias.
+ * Formats and prints an alias in the form 'name=value'.
+ * If the node is NULL, returns 1.
+ * Otherwise,
+ * Return: 0 after printing the alias.
  */
-int print_alias(list_t *node)
+int output_alias(list_t *node)
 {
 	char *equal_sign_position, *alias_name;
 
@@ -75,10 +80,10 @@ int print_alias(list_t *node)
 	{
 		equal_sign_position = _strchr(node->str, '=');
 		for (alias_name = node->str; alias_name <= equal_sign_position; alias_name++)
-			_putchar(*alias_name); // Print the alias name.
+			_putchar(*alias_name);
 
 		_putchar('\'');
-		_puts(equal_sign_position + 1); // Print the alias value.
+		_puts(equal_sign_position + 1);
 		_puts("'\n");
 		return (0);
 	}
@@ -87,27 +92,29 @@ int print_alias(list_t *node)
 
 /**
  * manage_alias - handles alias creation, updating, and printing
- * @info: structure containing shell state, including the alias list and argument vector
+ * @info: structure containing shell state, including the
+ * alias list and argument vector
  *
- * Processes the alias-related commands, creating, updating, or printing aliases based on the input arguments.
- * Returns 0 after processing the commands.
+ * Processes the alias-related commands, creating, updating,
+ * or printing aliases based on the input arguments.
+ * Return: 0 after processing the commands.
  */
-int _myalias(info_t *info)
+int manage_alias(info_t *info)
 {
-	if (info->argc == 1) // If there are no additional arguments, print all aliases.
+	if (info->argc == 1)
 	{
 		for (list_t *node = info->alias; node; node = node->next)
-			print_alias(node); // Call print_alias for each node in the alias list.
+			print_alias(node);
 	}
 	else
 	{
-		for (int i = 1; info->argv[i]; i++) // For each argument provided to alias...
+		for (int i = 1; info->argv[i]; i++)
 		{
 			char *equal_sign_position = _strchr(info->argv[i], '=');
 			if (equal_sign_position)
-				set_alias(info, info->argv[i]); // If there's an '=', attempt to set the alias.
+				set_alias(info, info->argv[i]);
 			else
-				print_alias(node_starts_with(info->alias, info->argv[i], '=')); // Otherwise, print the specified alias.
+				print_alias(node_starts_with(info->alias, info->argv[i], '='));
 		}
 	}
 
