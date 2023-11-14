5358d0b25b719ba204d7b8d75a2881fda58c91cb
Amakalu Vitalis <amakaluvitalis202@gmail.com>
2023-11-14 05:46:36 +0300

    Update getinfo.c

--- getinfo.c
+++ getinfo.c
@@ -12,7 +12,7 @@
  */
 void release_info_resources(info_t *info, int all)
 {
-	ffree(info->argv);
+	bfree(info->argv);
 	info->argv = NULL;
 	info->path = NULL;
 	if (all)
@@ -30,7 +30,7 @@ void release_info_resources(info_t *info, int all)
 		bfree((void **)info->cmd_buf);
 		if (info->readfd > 2)
 			close(info->readfd);
-		_putchar(BUF_FLUSH);
+		putchar(BUF_FLUSH);
 	}
 }
 
@@ -52,20 +52,20 @@ void establish_info(info_t *info, char **av)
 	info->fname = av[0];
 	if (info->arg)
 	{
-		info->argv = strtow(info->arg, " \t");
+		info->argv = strtok(info->arg, " \t");
 		if (!info->argv)
 		{
 			info->argv = malloc(sizeof(char *) * 2);
 			if (info->argv)
 			{
-				info->argv[0] = _strdup(info->arg);
+				info->argv[0] = strdup(info->arg);
 				info->argv[1] = NULL;
 			}
 		}
 		for (i = 0; info->argv && info->argv[i]; i++)
 		{
 		info->argc = i;
-		replace_alias(info);
+		remove_alias(info);
 		replace_vars(info);
 		}
 	}
