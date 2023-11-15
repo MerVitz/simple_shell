92bd5e97d0d5d75f584111b02b27a3a329879e67
Amakalu Vitalis <amakaluvitalis202@gmail.com>
2023-11-16 00:51:31 +0300

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -1,65 +1,14 @@
 #include "shell.h"
 
 /**
- * divide_string - breaks up a string into an array of words
+ * strtow - breaks up a string into an array of words
  * based on a single character delimiter
  * @str: the string to be tokenized
  * @d: the delimiter character used to tokenize the string
  * Return: a dynamically allocated array of tokenized strings,
  * or NULL if memory allocation fails
  */
-char **divide_string(char *str, char d)
-{
-	int i, j, k, m, numwords = 0;
-	char **s;
-
-	if (str == NULL || str[0] == 0)
-		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
-		if ((str[i] != d && str[i + 1] == d) ||
-		    (str[i] != d && !str[i + 1]))
-			numwords++;
-	if (numwords == 0)
-		return (NULL);
-	s = malloc((1 + numwords) * sizeof(char *));
-	if (!s)
-		return (NULL);
-	for (i = 0, j = 0; j < numwords; j++)
-	{
-		while (str[i] == d)
-			i++;
-		k = 0;
-		while (str[i + k] != d && str[i + k])
-			k++;
-		s[j] = malloc((k + 1) * sizeof(char));
-		if (!s[j])
-		{
-			for (k = 0; k < j; k++)
-				free(s[k]);
-			free(s);
-			return (NULL);
-		}
-		for (m = 0; m < k; m++)
-			s[j][m] = str[i++];
-		s[j][m] = 0;
-	}
-	s[j] = NULL;
-	return (s);
-}
-
-/**
- * segment_string - dissects a string into words based on a set of delimiters
- * @str: the string to be divided
- * @d: the string containing delimiter characters
- * Return: an array of words derived from the string, or
- * NULL if the operation fails
- *
- * This function analyzes a string and separates it into an array
- * of words, ignoring
- * consecutive delimiters and only considering non-delimiter
- * sequences as words.
- */
-char **segment_string(char *str, char *d)
+char **strtow(char *str, char *d)
 {
 	int i, j, k, m, numwords = 0;
 	char **s;
@@ -83,6 +32,57 @@ char **segment_string(char *str, char *d)
 			i++;
 		k = 0;
 		while (!is_delim(str[i + k], d) && str[i + k])
+			k++;
+		s[j] = malloc((k + 1) * sizeof(char));
+		if (!s[j])
+		{
+			for (k = 0; k < j; k++)
+				free(s[k]);
+			free(s);
+			return (NULL);
+		}
+		for (m = 0; m < k; m++)
+			s[j][m] = str[i++];
+		s[j][m] = 0;
+	}
+	s[j] = NULL;
+	return (s);
+}
+
+/**
+ * strtow2 - dissects a string into words based on a set of delimiters
+ * @str: the string to be divided
+ * @d: the string containing delimiter characters
+ * Return: an array of words derived from the string, or
+ * NULL if the operation fails
+ *
+ * This function analyzes a string and separates it into an array
+ * of words, ignoring
+ * consecutive delimiters and only considering non-delimiter
+ * sequences as words.
+ */
+char **strtow2(char *str, char d)
+{
+	int i, j, k, m, numwords = 0;
+	char **s;
+
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		if ((str[i] != d && str[i + 1] == d) ||
+		    (str[i] != d && !str[i + 1]) || str[i + 1] == d)
+			numwords++;
+	if (numwords == 0)
+		return (NULL);
+	s = malloc((1 + numwords) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (i = 0, j = 0; j < numwords; j++)
+	{
+		while (str[i] == d && str[i] != d)
+			i++;
+		k = 0;
+		while (str[i + k] != d && str[i + k] && str[i + k] != d)
 			k++;
 		s[j] = malloc((k + 1) * sizeof(char));
 		if (!s[j])
