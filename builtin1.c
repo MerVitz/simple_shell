6bde4eb552a0904f1588de4507eb252b70b920b5
Amakalu Vitalis <amakaluvitalis202@gmail.com>
2023-11-15 23:48:26 +0300

    Update builtin1.c

--- builtin1.c
+++ builtin1.c
@@ -1,11 +1,12 @@
 #include "shell.h"
 
 /**
- * _myhistory - displays the history list, one command by line, preceded
- *              with line numbers, starting at 0.
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
+ * _myhistory - outputs the history list with line numbers
+ * @info: structure containing shell state, including the history list
+ *
+ * Iterates through the history list and prints each command
+ * with its corresponding line number.
+ * Return: 0 after printing the entire history list.
  */
 int _myhistory(info_t *info)
 {
@@ -14,34 +15,13 @@ int _myhistory(info_t *info)
 }
 
 /**
- * unset_alias - sets alias to string
- * @info: parameter struct
- * @str: the string alias
+ * set_alias - creates a new alias or updates an existing one
+ * @info: structure containing shell state, including the alias list
+ * @str: string that represents the alias and its value
  *
- * Return: Always 0 on success, 1 on error
- */
-int unset_alias(info_t *info, char *str)
-{
-	char *p, c;
-	int ret;
-
-	p = _strchr(str, '=');
-	if (!p)
-		return (1);
-	c = *p;
-	*p = 0;
-	ret = delete_node_at_index(&(info->alias),
-		get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
-	*p = c;
-	return (ret);
-}
-
-/**
- * set_alias - sets alias to string
- * @info: parameter struct
- * @str: the string alias
- *
- * Return: Always 0 on success, 1 on error
+ * Analyzes the given string and either creates a new alias
+ * or updates an existing one.
+ * Return: 0 on success, 1 on failure to set the alias.
  */
 int set_alias(info_t *info, char *str)
 {
@@ -58,10 +38,13 @@ int set_alias(info_t *info, char *str)
 }
 
 /**
- * print_alias - prints an alias string
- * @node: the alias node
+ * print_alias - prints a formatted representation of an alias
+ * @node: the list node containing the alias string
  *
- * Return: Always 0 on success, 1 on error
+ * Formats and prints an alias in the form 'name=value'.
+ * If the node is NULL, returns 1.
+ * Otherwise,
+ * Return: 0 after printing the alias.
  */
 int print_alias(list_t *node)
 {
@@ -81,11 +64,14 @@ int print_alias(list_t *node)
 }
 
 /**
- * _myalias - mimics the alias builtin (man alias)
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: Always 0
- */
+ * _myalias - handles alias creation, updating, and printing
+ * @info: structure containing shell state, including the
+ * alias list and argument vector
+ *
+ * Processes the alias-related commands, creating, updating,
+ * or printing aliases based on the input arguments.
+ * Return: 0 after processing the commands.
+ *//
 int _myalias(info_t *info)
 {
 	int i = 0;
@@ -113,3 +99,31 @@ int _myalias(info_t *info)
 
 	return (0);
 }
+
+/**
+ * unset_alias - deletes an alias from the list of aliases
+ * @info: structure containing shell state, including the alias list
+ * @str: string that represents the alias to be removed
+ *
+ * Separates the alias name from its value and removes the alias
+ * from the list if found.
+ * Return: 0 on successful removal, 1 if the alias was not found
+ * or if an error occurred.
+ */
+int unset_alias(info_t *info, char *str)
+{
+	char *p, c;
+	int ret;
+
+	p = _strchr(str, '=');
+	if (!p)
+		return (1);
+	c = *p;
+	*p = 0;
+	ret = delete_node_at_index(&(info->alias),
+		get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
+	*p = c;
+	return (ret);
+}
+
+
