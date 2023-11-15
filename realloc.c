f066861a19b223de7fdd34cef808fae7f7bebfa1
Amakalu Vitalis <amakaluvitalis202@gmail.com>
2023-11-15 22:35:49 +0300

    Update realloc.c

--- realloc.c
+++ realloc.c
@@ -1,11 +1,11 @@
 #include "shell.h"
 
 /**
- **_memset - fills memory with a constant byte
- *@s: the pointer to the memory area
- *@b: the byte to fill *s with
- *@n: the amount of bytes to be filled
- *Return: (s) a pointer to the memory area s
+ * _memset - sets a block of memory to a specified value
+ * @s: pointer to the start of the memory block
+ * @b: the value to set in the memory block
+ * @n: the number of bytes to set in the memory block
+ * Return: pointer to the beginning of the memory block
  */
 char *_memset(char *s, char b, unsigned int n)
 {
@@ -17,8 +17,11 @@ char *_memset(char *s, char b, unsigned int n)
 }
 
 /**
- * ffree - frees a string of strings
- * @pp: string of strings
+ * ffree - deallocates a dynamically allocated array of strings
+ * @pp: array of string pointers to be deallocated
+ *
+ * This function iterates through an array of strings, freeing each one,
+ * Return: frees the array itself.
  */
 void ffree(char **pp)
 {
@@ -32,12 +35,13 @@ void ffree(char **pp)
 }
 
 /**
- * _realloc - reallocates a block of memory
- * @ptr: pointer to previous malloc'ated block
- * @old_size: byte size of previous block
- * @new_size: byte size of new block
+ * _realloc - changes the size of a previously allocated memory block
+ * @ptr: reference to the memory block originally allocated with malloc
+ * @old_size: the size of the allocated space for ptr
+ * @new_size: the new size of the allocated space
  *
- * Return: pointer to da ol'block nameen.
+ * Return: a pointer to the newly allocated memory, which is suitably
+ * aligned for any kind of variable and may be different from ptr.
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
