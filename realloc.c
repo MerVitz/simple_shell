b891d7946dd1a50c717133a6a653199801868223
Amakalu Vitalis <amakaluvitalis202@gmail.com>
2023-11-15 21:42:36 +0300

    Update realloc.c

--- realloc.c
+++ realloc.c
@@ -1,13 +1,13 @@
 #include "shell.h"
 
 /**
- * fill_memory - sets a block of memory to a specified value
- * @s: pointer to the start of the memory block
- * @b: the value to set in the memory block
- * @n: the number of bytes to set in the memory block
- * Return: pointer to the beginning of the memory block
+ **_memset - fills memory with a constant byte
+ *@s: the pointer to the memory area
+ *@b: the byte to fill *s with
+ *@n: the amount of bytes to be filled
+ *Return: (s) a pointer to the memory area s
  */
-char *fill_memory(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
@@ -17,13 +17,10 @@ char *fill_memory(char *s, char b, unsigned int n)
 }
 
 /**
- * array_free - deallocates a dynamically allocated array of strings
- * @pp: array of string pointers to be deallocated
- *
- * This function iterates through an array of strings, freeing each one,
- * Return: frees the array itself.
+ * ffree - frees a string of strings
+ * @pp: string of strings
  */
-void array_free(char **pp)
+void ffree(char **pp)
 {
 	char **a = pp;
 
@@ -35,15 +32,14 @@ void array_free(char **pp)
 }
 
 /**
- * resize_memory - changes the size of a previously allocated memory block
- * @ptr: reference to the memory block originally allocated with malloc
- * @old_size: the size of the allocated space for ptr
- * @new_size: the new size of the allocated space
+ * _realloc - reallocates a block of memory
+ * @ptr: pointer to previous malloc'ated block
+ * @old_size: byte size of previous block
+ * @new_size: byte size of new block
  *
- * Return: a pointer to the newly allocated memory, which is suitably
- * aligned for any kind of variable and may be different from ptr.
+ * Return: pointer to da ol'block nameen.
  */
-void *resize_memory(void *ptr, unsigned int old_size, unsigned int new_size)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *p;
 
@@ -59,8 +55,8 @@ void *resize_memory(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 
 	old_size = old_size < new_size ? old_size : new_size;
-	for (unsigned int i = 0; i < old_size; i++)
-		p[i] = ((char *)ptr)[i];
+	while (old_size--)
+		p[old_size] = ((char *)ptr)[old_size];
 	free(ptr);
 	return (p);
 }
