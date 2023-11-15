b1cc4c61aa5f8b90b55503bdad139706fbcc07ac
Amakalu Vitalis <amakaluvitalis202@gmail.com>
2023-11-15 21:30:21 +0300

    Update exits.c

--- exits.c
+++ exits.c
@@ -1,79 +1,74 @@
 #include "shell.h"
 
 /**
- * copy_partial_string - replicates a subset of a source
- * string to a destination
- * @dest: the target buffer for copying
- * @src: the original string from which to copy characters
- * @n: the count of characters to replicate
- *
- * This function will copy 'n' characters from 'src' to 'dest'.
- * If 'src' has fewer than 'n'
- * characters, the remainder of 'dest' will be padded with null bytes.
- * Return: a pointer to the destination string.
+ **_strncpy - copies a string
+ *@dest: the destination string to be copied to
+ *@src: the source string
+ *@n: the amount of characters to be copied
+ *Return: the concatenated string
  */
-char *copy_partial_string(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0;
+	int i, j;
+	char *s = dest;
 
-	while (src[index] && index < n - 1)
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
 	{
-		dest[index] = src[index];
-		index++;
+		dest[i] = src[i];
+		i++;
 	}
-	for (int padding = index; padding < n; padding++)
-		dest[padding] = '\0';
-
-	return (dest);
-}
-
-/**
- * append_limited_chars - appends characters from one string
- * to another up to a limit
- * @dest: the string to be appended to
- * @src: the string to append from
- * @n: the maximum number of characters to transfer
- *
- * Concatenates up to 'n' characters from 'src' onto the end
- * of 'dest'. If 'src' is shorter
- * than 'n', the concatenation stops at the null-terminator.
- * The destination string is always
- * null-terminated.
- * Return: the concatenated string.
- */
-char *append_limited_chars(char *dest, char *src, int n)
-{
-	int dest_len = 0, src_index = 0;
-
-	while (dest[dest_len])
-		dest_len++;
-	while (src[src_index] && src_index < n)
-		dest[dest_len++] = src[src_index++];
-	if (src_index < n)
-		dest[dest_len] = '\0';
-
-	return (dest);
-}
-
-/**
- * find_char - locates the first instance of a character in a string
- * @s: the string within which to search for the character
- * @c: the target character to find
- *
- * Searches through a string to find the first appearance of
- * a specified character, 'c'.
- * If the character is found, a pointer to its location in the
- * string is returned. If the character
- * Return: NULL.
- */
-char *find_char(char *s, char c)
-{
-	while (*s)
+	if (i < n)
 	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: the amount of bytes to be maximally used
+ *Return: the concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
 		if (*s == c)
 			return (s);
-		s++;
-	}
+	} while (*s++ != '\0');
 
 	return (NULL);
 }
