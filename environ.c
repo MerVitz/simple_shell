2dd58da96b262c85cd912430abc8f73ce636e75f
Amakalu Vitalis <amakaluvitalis202@gmail.com>
2023-11-16 01:12:51 +0300

    Update environ.c

--- environ.c
+++ environ.c
@@ -1,20 +1,6 @@
 #include "shell.h"
 
 /**
- * _myenv - prints the current environment to stdout
- * @info: shell state information including the env list
- *
- * Outputs the entire environment list to stdout, using the
- * print_list_str function.
- * Return: 0 always.
- */
-int _myenv(info_t *info)
-{
-	print_list_str(info->env);
-	return (0);
-}
-
-/**
  * _getenv - obtains the value of an environment variable
  * @info: shell state information, used for accessing the env list
  * @name: the name of the environment variable to find
@@ -39,6 +25,26 @@ char *_getenv(info_t *info, const char *name)
 }
 
 /**
+ * populate_env_list - creates a linked list from the environment variables
+ * @info: shell state information, including the head of the env list
+ *
+ * Loops over the system environment variables
+ * and appends each one to a new linked list.
+ * The head of the list is stored in the info structure.
+ * Return: 0.
+ */
+int populate_env_list(info_t *info)
+{
+	list_t *node = NULL;
+	size_t i;
+
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
+	info->env = node;
+	return (0);
+}
+
+/**
  * _mysetenv - removes environment variables
  * @info: shell state information including the env list and argument count
  *
@@ -57,6 +63,20 @@ int _mysetenv(info_t *info)
 	if (_setenv(info, info->argv[1], info->argv[2]))
 		return (0);
 	return (1);
+}
+
+/**
+ * _myenv - prints the current environment to stdout
+ * @info: shell state information including the env list
+ *
+ * Outputs the entire environment list to stdout, using the
+ * print_list_str function.
+ * Return: 0 always.
+ */
+int _myenv(info_t *info)
+{
+	print_list_str(info->env);
+	return (0);
 }
 
 /**
@@ -80,25 +100,5 @@ int _myunsetenv(info_t *info)
 	for (i = 1; i <= info->argc; i++)
 		_unsetenv(info, info->argv[i]);
 
-	return (0);
-}
-
-/**
- * populate_env_list - creates a linked list from the environment variables
- * @info: shell state information, including the head of the env list
- *
- * Loops over the system environment variables
- * and appends each one to a new linked list.
- * The head of the list is stored in the info structure.
- * Return: 0.
- */
-int populate_env_list(info_t *info)
-{
-	list_t *node = NULL;
-	size_t i;
-
-	for (i = 0; environ[i]; i++)
-		add_node_end(&node, environ[i], 0);
-	info->env = node;
 	return (0);
 }
