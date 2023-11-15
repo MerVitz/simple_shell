f08680ec3be6d1ef3b10e4a3347e3f5cedee107f
Amakalu Vitalis <amakaluvitalis202@gmail.com>
2023-11-15 21:45:13 +0300

    Update string1.c

--- string1.c
+++ string1.c
@@ -1,68 +1,63 @@
 #include "shell.h"
 
 /**
- * replicate_string - duplicates the content of one string to another location
- * @dest: target location where the string will be copied
- * @src: source string to be copied
+ * _strcpy - copies a string
+ * @dest: the destination
+ * @src: the source
  *
- * This function replicates the content of the source string
- * into the destination buffer,
- * ensuring the destination has a copy of the string.
- * Return: the destination pointer.
+ * Return: pointer to destination
  */
-char *replicate_string(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	if (dest == src || !src)
+	if (dest == src || src == 0)
 		return (dest);
 	while (src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
+	dest[i] = 0;
 	return (dest);
 }
 
 /**
- * generate_duplicate - generates a new memory allocation
- * containing the same string
- * @str: the original string to duplicate
+ * _strdup - duplicates a string
+ * @str: the string to duplicate
  *
- * This function allocates new memory and copies the input string into it,
- * Return: a pointer to the newly allocated duplicate.
+ * Return: pointer to the duplicated string
  */
-char *generate_duplicate(const char *str)
+char *_strdup(const char *str)
 {
 	int length = 0;
 	char *ret;
 
-	if (!str)
+	if (str == NULL)
 		return (NULL);
-	while (str[length])
+	while (*str++)
 		length++;
 	ret = malloc(sizeof(char) * (length + 1));
 	if (!ret)
 		return (NULL);
-	strcpy(ret, str);
+	for (length++; length--;)
+		ret[length] = *--str;
 	return (ret);
 }
 
 /**
- * echo_string - prints a given string to standard output
- * @str: the string to be echoed
+ *_puts - prints an input string
+ *@str: the string to be printed
  *
- * By invoking the character-wise output function,
- * Return: writes an entire string to stdout.
+ * Return: Nothing
  */
-void echo_string(char *str)
+void _puts(char *str)
 {
 	int i = 0;
 
 	if (!str)
 		return;
-	while (str[i])
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
@@ -70,16 +65,13 @@ void echo_string(char *str)
 }
 
 /**
- * buffered_char_output - writes characters to stdout with
- * buffering capabilities
- * @c: character to be printed, or control character to manage the buffer
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * This function manages a static buffer to collect characters
- * and writes them to stdout
- * in a batch. It's designed to be efficient by reducing
- * Return: number of write calls.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int buffered_char_output(char c)
+int _putchar(char c)
 {
 	static int i;
 	static char buf[WRITE_BUF_SIZE];
